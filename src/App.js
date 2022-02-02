4cd321e4f34d1c79db2ef1f4dbea5d2ea9fbeae2
Your Name <you@example.com>
2022-02-02 20:36:19 +0000

    checkpoint

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./Components/Navbar";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import CocktailList from "./Pages/CocktailList";
+import About from "./Pages/About";
+import Error from "./Pages/Error";
+import CocktailDetail from "./Pages/CocktailDetail";
+import { useState } from "react";
 
 function App() {
+  const [search, setSearch] = useState("");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar setSearch={setSearch} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/CocktailList"
+          element={<CocktailList search={search} />}
+        />
+        <Route path="/CocktailList/:idDrink" element={<CocktailDetail />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/*" element={<Error />} />
+      </Routes>
     </div>
   );
 }
